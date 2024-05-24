d192797a32997eaa24ad025dcac22dff91a86b10
Peter Schilling <peter@peterschilling.org>
2024-05-24 08:06:09 -0700

    basics

---
+++ src/cli.ts
@@ -0,0 +1,22 @@
+import { Clerc } from "clerc";
+import { helpPlugin } from "@clerc/plugin-help";
+import { versionPlugin } from "@clerc/plugin-version";
+import { notFoundPlugin } from "@clerc/plugin-not-found";
+import { strictFlagsPlugin } from "@clerc/plugin-strict-flags";
+
+Clerc.create()
+  .scriptName("srcset-codegen")
+  .description("Generates srcset")
+  .version("0.0.1")
+  .use(helpPlugin())
+  .use(versionPlugin())
+  .use(notFoundPlugin())
+  .use(strictFlagsPlugin())
+  .command("generate", "Generate ts files for all images", {
+    parameters: ["<directory>"],
+  })
+  .on("generate", async (context) => {
+    let directory = context.parameters.directory;
+    console.log("Generating srcset in dir: ", directory);
+  })
+  .parse();
