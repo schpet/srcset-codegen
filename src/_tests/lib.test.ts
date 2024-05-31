b6c79f773300101fb17dcf393df210c92d3a570a
Peter Schilling <peter@peterschilling.org>
2024-05-31 07:25:43 -0700

    rewrite tests to vitest

--- src/_tests/lib.test.ts
+++ src/_tests/lib.test.ts
@@ -1,11 +1,10 @@
-import assert from "node:assert"
-import test from "node:test"
+import { expect, test } from 'vitest'
 import * as lib from "../lib"
 
 test("generate", async () => {
 	let result = await lib.generateCode({ directory: "src/_tests/fixtures" })
 	let keys = Array.from(result.keys())
-	assert.deepEqual(keys, [
+	expect(keys).toEqual([
 		"src/_tests/fixtures/single.png",
 		"src/_tests/fixtures/smile.png",
 		"src/_tests/fixtures/vector.svg",
